60071b73aa2df882fa13febd56437be5a1a9bedf
Protesilaos Stavrou <info@protesilaos.com>
2023-02-10 10:25:28 +0200

    Simplify outline-regexp

--- sxhkdrc-mode.el
+++ sxhkdrc-mode.el
@@ -52,7 +52,7 @@ Space Partitioning Window Manager (BSPWM)."
                       "mod1" "mod2" "mod3" "mod4" "mod5"))
     (key-generic . "^\\({.*?}\\|\\<.*?\\>\\)")
     (key-line . "^\\({.*?}\\|\\<.*?\\>\\).*$")
-    (outline . "^\\(\\({.*?}\\|\\<.*?\\>\\)\\|###+ \\)")
+    (outline . "^###+ ")
     (comment . "^\\([\s\t]+\\)?#.*$")
     (command . "^[\s\t]+\\([;]\\)?\\(\\_<.*?\\_>\\)")
     (command-line . "^[\s\t]+\\([;]\\)?\\(\\_<.*?\\_>\\).*$")
