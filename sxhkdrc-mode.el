8d571ee02967622afe3bfdd86e433402279f395d
Protesilaos Stavrou <info@protesilaos.com>
2026-01-11 12:54:25 +0200

    Update the copyright years

--- sxhkdrc-mode.el
+++ sxhkdrc-mode.el
@@ -1,6 +1,6 @@
 ;;; sxhkdrc-mode.el --- Major mode for sxhkdrc files (Simple X Hot Key Daemon) -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2025  Free Software Foundation, Inc.
+;; Copyright (C) 2022-2026  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
